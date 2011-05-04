94ba83ac2887207f11d31cace2a413902bb5decd
Stephen Moseson <smoseson@gmail.com>
2011-05-04 17:18:21 -0400

    Added java project

---
+++ code/src/Ctalkerithm.java
@@ -0,0 +1,18 @@
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.sparql.vocabulary.FOAF;
+import com.hp.hpl.jena.vocabulary.*;
+
+public class Ctalkerithm {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
