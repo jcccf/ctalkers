66c07fe6e14e985a90353cb8647a9834bde4e617
Heran Yang <heranyang@gmail.com>
2011-05-14 11:45:39 -0400

    model done

--- code/src/Database.java
+++ code/src/Database.java
@@ -9,6 +9,7 @@ import com.hp.hpl.jena.query.QueryExecution ;
 import com.hp.hpl.jena.query.QueryExecutionFactory ;
 import com.hp.hpl.jena.query.QueryFactory ;
 import com.hp.hpl.jena.query.ResultSetFormatter ;
+import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.sparql.core.Quad ;
 import com.hp.hpl.jena.sparql.sse.SSE ;
 import com.hp.hpl.jena.tdb.TDB ;
@@ -17,6 +18,7 @@ import com.hp.hpl.jena.tdb.nodetable.NodeTable ;
 import com.hp.hpl.jena.tdb.store.DatasetGraphTDB ;
 import com.hp.hpl.jena.tdb.store.NodeId ;
 import com.hp.hpl.jena.tdb.sys.SystemTDB ;
+import com.hp.hpl.jena.vocabulary.ResultSet;
 
 /** Example of how to filter quads as they are accessed at the lowest level.
  * Can be used to exclude daat from specific graphs.   
@@ -30,7 +32,15 @@ public class Database
 {
     private static String graphToHide = "http://example/g2" ;
 
-    public static void main(String ... args)
+    public static void main(String[] args) {
+    	String fldr = "../TDB-0.8.10/work/data/project2";
+    	Model model = TDBFactory.createModel(fldr);
+    	
+    	model.close();
+    }
+    
+    
+    public static void mai(String ... args)
     {
         // This also works for default union graph ....
         TDB.getContext().setTrue(TDB.symUnionDefaultGraph) ;
@@ -39,6 +49,9 @@ public class Database
         Filter<Tuple<NodeId>> filter = createFilter(ds) ;
         example(ds, filter) ;
     }
+    
+
+
     
     /** Example setup - in-memory dataset with two graphs, one triple in each */
     private static Dataset setup()
@@ -120,30 +133,3 @@ public class Database
     }
         
 }
-
-/*
- * (c) Copyright 2010 Epimorphics Ltd.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
