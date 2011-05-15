8ac6befa23c5f8d1a02431b0caa7e2a62d047087
Stephen Moseson <smoseson@gmail.com>
2011-05-15 17:34:19 -0400

    Moved dbp location searching to Location.java

--- code/src/Location.java
+++ code/src/Location.java
@@ -1,3 +1,10 @@
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+
 
 
 public class Location {
@@ -14,10 +21,7 @@ public class Location {
 		name = n;
 	}
 	
-	public Location(String n, String[] f) {
-		name = n;
-		
-		
+	public void addLongAndLat(String[] f) {	
 		double[] ls = new double[8];
 		for (int i = 0; i < 8; i++) {
 			if(i != 2 && i != 5)
@@ -43,6 +47,83 @@ public class Location {
 		populationTotal + "," +
 		areaTotal + "}";
 	}
+	
+	
+	// null if dbpedia lookup returns no city or country
+	public static Location createLocation(String locName) {
+		
+		// Search DBpedia for location information
+		
+		// name spaces
+		String dbpResource = "http://dbpedia.org/resource/";
+		String dbpProp = "http://dbpedia.org/property/";
+		String dbpOwl = "http://dbpedia.org/ontology/";
+		
+		String dbpSparql = "http://dbpedia.org/sparql";
+		
+		// try extracting country
+		Location loc;
+		try {
+			
+			
+			
+			
+			
+			
+			loc = new Location(locName);
+			//loc.c
+		} catch(Exception e) {}
+		
+		// try extracting city
+
+		
+		
+		// try adding longitude, latitude, total pop, and total area
+		try {
+			// Extract the following triples from each location
+			String [] props = {"latd", "latm", "latns", "lats", "longd", "longew", "longm", "longs"};
+			String [] owlProps = {"populationTotal", "areaTotal"};
+			
+			// Create the prop query string
+			String queryString = "SELECT ?" + Utils.join(props, " ?") + " ?" + Utils.join(owlProps, " ?") + "\nWHERE {\n";
+			for(String prop: props) {
+				queryString += "<" + dbpResource + locName + "> <" + dbpProp + prop + "> ?" + prop + ".\n";
+			}
+			for(String owlProp:owlProps) {
+				queryString += "<" + dbpResource + locName + "> <" + dbpOwl + owlProp + "> ?" + owlProp + ".\n";
+			}
+			queryString += "}";
+	
+			System.out.println(queryString);
+			Query query = QueryFactory.create(queryString);
+	
+			// Execute the query and obtain results
+			QueryExecution qe = QueryExecutionFactory.sparqlService(dbpSparql, query);
+			ResultSet results = qe.execSelect();
+	
+			QuerySolution res = results.next();
+			
+			String[] locFields = new String[Location.fieldCount];
+			int k=0;
+			for(String prop: props) {
+				locFields[k++] = res.get(prop).toString();
+			}
+			for(String owlProp: owlProps) {
+				locFields[k++] = res.get(owlProp).toString();
+			}
+			
+			loc.addLongAndLat(locFields);
+			
+			qe.close();
+		} catch(Exception e) {
+			
+		}
+		
+		
+		return loc;
+
+	}
+	
 
 }
 
